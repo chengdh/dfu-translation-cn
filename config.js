41f36e28db4c601d27a47703414bdceb35b41300
chengdh <542340109@qq.com>
2024-01-19 10:39:40 +0000

    翻译 csv 文件成功

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 export const YOUDAO_API_BASE_URL = "https://openapi.youdao.com"
 export const YOUDAO_APP_ID = "14c570c81fc6f376"
 export const YOUDAO_APP_SECRET = "Ojnfzjxo03PGakqIhYxKUL5tEl0OdrNS"
+export const YOUDAO_VOCABID = "98DE7A664DF14604A9FBEFF791F86892"
--- index.js
+++ index.js
@@ -4,16 +4,16 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as csv from 'fast-csv'
 import { writeToPath } from '@fast-csv/format'
-import { YOUDAO_API_BASE_URL, YOUDAO_APP_ID, YOUDAO_APP_SECRET } from "./config.js"
+import { YOUDAO_API_BASE_URL, YOUDAO_APP_ID, YOUDAO_APP_SECRET, YOUDAO_VOCABID } from "./config.js"
 
 const translate_csv = () => {
     const csv_path = path.resolve('./text', "Master Localization CSV Files")
     const translated_csv_path = path.resolve('./cn', "Master Localization CSV Files")
     fs.readdir(csv_path, async function (err, files) {
         for (const f of files) {
-            // if (f.endsWith(".csv")) {
-            if (f === "Example_MageLight.csv") {
-                const translated_arry = [["Key","Value"]]
+            if (f.endsWith(".csv")) {
+            // if (f === "Example_MageLight.csv") {
+                const translated_arry = [["Key", "Value"]]
                 const file = `${csv_path}/${f}`
                 let total_row_count = 0
                 let translated_row_count = 0
@@ -32,7 +32,7 @@ const translate_csv = () => {
                             console.log("translated_row_count: ", translated_row_count)
                             console.log("total_row_count : ", translated_row_count)
                             console.log("translated_arry:", translated_arry)
-                            writeToPath(`${translated_csv_path}`, translated_arry)
+                            writeToPath(`${translated_csv_path}/${f}`, translated_arry)
                                 .on('error', err => console.error(err))
                                 .on('finish', () => console.log('Done writing.'));
                         }
@@ -71,7 +71,7 @@ const translate = async (q, from, to) => {
     const curtime = Math.round(new Date().getTime() / 1000);
     const str = YOUDAO_APP_ID + truncate(q) + salt + curtime + YOUDAO_APP_SECRET;
     const sign = CryptoJS.SHA256(str).toString(CryptoJS.enc.Hex);
-    const vocabId = '您的用户词表ID';
+    const vocabId = YOUDAO_VOCABID
     const data = {
         q,
         appKey: YOUDAO_APP_ID,
@@ -80,7 +80,9 @@ const translate = async (q, from, to) => {
         to,
         sign,
         signType: "v3",
+        strict: "true",
         curtime,
+        vocabId
     }
     const headers = {
         "Content-Type": "multipart/form-data"
@@ -93,15 +95,18 @@ const translate = async (q, from, to) => {
         data
 
     })
-    // console.log("translate:", resp)
+    console.log("translate:", resp)
     if (resp.errorCode != "0") {
         console.error("translate error:", q)
         return null
     }
     return resp.translation[0]
 }
-// const text = "hello world!"
-// const from = "en"
-// const to = "zh-CHS"
+// const text = "Duration: %bdr + %adr per %cld level(s)"
+// const text = "Store is closed. Open from %d1:00 to %d2:00."
+// const text ="nteractionIsNowInMode, Interaction is now in %s mode."
+// const text = "Duration: %s seconds,%d number"
+const from = "en"
+const to = "zh-CHS"
 // translate(text, from, to)
 translate_csv()
